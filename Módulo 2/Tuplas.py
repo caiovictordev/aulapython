99555704be024f5bd2ea60f08ab6b7a8c5930ffc
caiovictordev <caiovictorsenai@gmail.com>
2025-07-30 10:11:47 -0300

    Lista python completo 1.1

---
+++ Módulo 2/Tuplas.py
@@ -0,0 +1,7 @@
+#Tuplas armazenam dados que não podem ser alterados
+
+equipamento = ("Monitor cardíaco", "SN-45678") #Sintaxe do Tupla
+
+#Você pode acessar os valores da tupla como em uma lista,
+# usando índices, mas não pode adicionar, remover ou modificar os itens depois que ela é criada.
+# Isso garante segurança e integridade para informações fixas.
