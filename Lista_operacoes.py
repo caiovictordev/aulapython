917bfb8d303b836036740522904b61d288895f40
caiovictordev <caiovictorsenai@gmail.com>
2025-07-30 10:00:02 -0300

    Lista python completo 1.1

--- Lista_operacoes.py
+++ Lista_operacoes.py
@@ -1,7 +1,7 @@
-pecas = ["válvula", "sensor", "motor"]
+pecas = ["válvula", "sensor", "motor"] #Lista seqêncial
 
-pecas.append("resistor")
+pecas.append("resistor") #adiciona um item ao final da lista.
 
-pecas.remove("sensor")
+pecas.remove("sensor") #remove a primeira ocorrência do item na lista.
 
 print(pecas)
