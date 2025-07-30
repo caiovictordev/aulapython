f617d80aa4c04d177583567d959e1ddf9421b6cc
caiovictordev <caiovictorsenai@gmail.com>
2025-07-30 10:45:00 -0300

    Tupla módulo 2

---
+++ Módulo 2/Tuplas/Tupla exemplo.py
@@ -0,0 +1,6 @@
+pecas_substituidas = ('parafuso', 'parafuso', 'corda', 'mouse')#sintaxe da tupla
+
+ #O "set" serve para eliminar itens duplicados
+pecas_unicas = set(pecas_substituidas) #Nessa sintaxe você pode ver isso
+
+print(pecas_unicas) #resultado: {'parafuso', 'corda', 'mouse'}
