41df5e98b4f94b5586ed56ecf5bee61b6e1cfda4
caiovictordev <caiovictorsenai@gmail.com>
2025-08-06 19:39:53 -0300

    Pre atividade

---
+++ Módulo 2/Estrurura de repetição/For.py
@@ -0,0 +1,4 @@
+pecas_substituidas = ["valvùlas"]
+
+for peca in pecas_substituidas:
+    print("A peça {peca} foi substituída.")
---
+++ Módulo 2/aula 1/meme.py
@@ -0,0 +1,5 @@
+nome = "cato"
+idade = 19
+valorNaConta = 0.10
+print(nome)
+print(f"Oi, meu nome é {nome}, tenho {idade}, anos e esse aqui ${valorNaConta}, é o valor na minha conta")
